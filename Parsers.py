2303ceb88c51e1cfb1d9e744bb4bce973aa15709
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-07-02 14:14:17 +0500

    Тестовые парсеры

---
+++ Parsers.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def search1(searchword):
+    html = requests.get('https://radugastroy.ru/search/?module=search&action=search&searchword=' + searchword).text
+    soup = BeautifulSoup(html)
+
+    categories = []
+    for category in soup.find_all("li", {"class": "shopListingPage-cat"}):
+        categories.append(category.find("a").text)
+
+    for block in soup.find_all("li", {"class": "shopBlock-item"}):
+        name = block.find("div", {"class": "name"}).find("a").text
+        price = block.find("b", {"class": "js_shop_price"}).text
+
+        in_stock = "+"
+        if block.find("div", {"class": "stock js_stock inStock"}) is None:
+            in_stock = "-"
+
+        print(name + " (" + price + ")  " + in_stock)
+
+
+def search2(searchword):
+    html = requests.get('https://www.sdvor.com/moscow/search/?only_in_stock=true&str=' + searchword).text
+    soup = BeautifulSoup(html)
+
+    categories = []
+    for category in soup.find("ul", {"class": "lkhh32y"}).find_all("li"):
+        category = category.find('a', {"class": "lf4rfcn l1f0l9hb a1pqe0q7"})
+        if category is None:
+            break
+        categories.append(category.text)
+
+    for block in soup.find_all("div", {"class": "w1l5ijvb p4t3w1l".split()}):
+        name = block.find("div", {"class": "i1b5ubz7"}).find("a").text
+        price = block.find("span", {"class": "p1hbhc78"}).text
+        in_stock = "+"
+        print(name + " (" + price + ")  " + in_stock)
+
+
+def search3(searchword):
+    html = requests.get('https://s-b-1.ru/catalog/?q=' + searchword).text
+    soup = BeautifulSoup(html)
+
+    categories = []
+    for category in soup.find("div", {"class": "top_block_filter_section"}) \
+            .find("div", {"class": "items"}).find_all("div", {"class": "item"}):
+        category = category.find('a')
+        if category is None:
+            break
+        categories.append(category.find("span").text)
+
+    for block in soup.find_all("div", {"class": "item_block"}):
+        name = block.find("div", {"class": "item-title"}).find("span").text
+        price = block.find("span", {"class": "price_value"}).text
+
+        item_stock = block.find("div", {"class": "item-stock"})
+
+        in_stock = "+"
+
+        if item_stock.find("span", {"class": "value"}).text == "Нет в наличии":
+            in_stock = "-"
+
+        print(name + " (" + price + ")  " + in_stock)
