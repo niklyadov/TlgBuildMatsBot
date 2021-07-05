5735ee6b0c6b560aa4d9909c2fde025c5c1d2608
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-07-05 22:22:17 +0500

    Парсеры

--- Parsers.py
+++ Parsers.py
@@ -22,10 +22,6 @@ class RadugastroyParser(Parser):
         html = requests.get('https://radugastroy.ru/search/?module=search&action=search&searchword=' + search_word).text
         soup = BeautifulSoup(html)
 
-        categories = []
-        for category in soup.find_all("li", {"class": "shopListingPage-cat"}):
-            categories.append(category.find("a").text)
-
         address = soup.find('div', {'class': 'siteFooter-address'}).text
 
         result = []
@@ -36,7 +32,7 @@ class RadugastroyParser(Parser):
             price = block.find("b", {"class": "js_shop_price"}).text
 
             if block.find("div", {"class": "stock js_stock inStock"}) is None:
-                return
+                continue
 
             result.append(RequestModel(
                 search_word,
@@ -60,19 +56,12 @@ class SdvorParser(Parser):
 
         addresses = []
         for city in cities:
-            for address in requests.get('https://www.sdvor.com/api/omni_order/shops/?city_id=' + city.id).text:
+            for address in requests.get('https://www.sdvor.com/api/omni_order/shops/?city_id=' + city['id']).text:
                 address = json.loads(address)
-                addresses.append(city.name + ' ' + address.address)
+                addresses.append(city['name'] + ' ' + address['address'])
 
         html = requests.get('https://www.sdvor.com/moscow/search/?only_in_stock=true&str=' + search_word).text
         soup = BeautifulSoup(html)
-
-        categories = []
-        for category in soup.find("ul", {"class": "lkhh32y"}).find_all("li"):
-            category = category.find('a', {"class": "lf4rfcn l1f0l9hb a1pqe0q7"})
-            if category is None:
-                break
-            categories.append(category.text)
 
         result = []
         for block in soup.find_all("div", {"class": "w1l5ijvb p4t3w1l".split()}):
@@ -90,6 +79,7 @@ class SdvorParser(Parser):
                 0,
                 addresses
             ))
+        return result
 
 
 class Sb1Parser(Parser):
@@ -99,26 +89,28 @@ class Sb1Parser(Parser):
         html = requests.get('https://s-b-1.ru/catalog/?q=' + search_word).text
         soup = BeautifulSoup(html)
 
-        categories = []
-        for category in soup.find("div", {"class": "top_block_filter_section"}) \
-                .find("div", {"class": "items"}).find_all("div", {"class": "item"}):
-            category = category.find('a')
-            if category is None:
-                break
-            categories.append(category.find("span").text)
-
+        result = []
         for block in soup.find_all("div", {"class": "item_block"}):
-            name = block.find("div", {"class": "item-title"}).find("span").text
+            link = block.find("div", {"class": "item-title"})
+            name = link.find("span").text
             price = block.find("span", {"class": "price_value"}).text
-
             item_stock = block.find("div", {"class": "item-stock"})
-
-            in_stock = "+"
+            url = link.get('href')
 
             if item_stock.find("span", {"class": "value"}).text == "Нет в наличии":
-                in_stock = "-"
+                continue
 
-            print(name + " (" + price + ")  " + in_stock)
+            result.append(RequestModel(
+                search_word,
+                url,
+                name,
+                price,
+                None,
+                0,
+                {}
+            ))
+
+        return result
 
 
 parsers = [RadugastroyParser, SdvorParser, Sb1Parser]
